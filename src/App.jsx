03bcae011e26d9bfb5e332443b4afa171e409eb1
Cicero <ciceropzr@gmail.com>
2025-01-24 14:39:18 -0300

    Criado estrutura de pastas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { Routes, Route } from "react-router";
+
+import Home from './pages/Home';
+import ProjetosDestaques from './pages/ProjetosDestaques';
+import Favoritos from './pages/Favoritos';
+
 function App() {
   return (
     <>
+    <Routes>
+        <Route index element={<Home />} />
+        <Route path="/projetos-destaques" element={<ProjetosDestaques />} />
+        <Route path="/favoritos" element={<Favoritos />} />
+      </Routes>
     </>
   )
 }
